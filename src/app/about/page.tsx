6cb6c2615cc21e83298a9dab8fd69a3ab374b68f
Andre <andre_ranulfo@hotmail.com>
2025-05-07 00:14:58 -0300

    MetaData

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
 
 import React from 'react'
+export const metadata = {
+  title: 'About',
+  description: 'About page',
+}
+
 
 export default function About() {
   return (
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,28 @@
 import React from 'react';
+import { Metadata } from 'next';
+
+type Props = {
+  params: { productId: string }
+}
+
+// Async function to generate metadata based on route params
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const id = params.productId;
+
+  const title  = await new Promise ((resolve)=>{
+    setTimeout(()=>{
+      resolve(`-- Product Id: ${id}`)
+
+      ,100})
+  } )
+
+  return {
+    title: `${title}`,
+   
+  };
+};
+
+ 
 
 export default async function ProductDetails({ params }: { params: Promise<{ productId: string }> }) {
   const { productId } = await params;
