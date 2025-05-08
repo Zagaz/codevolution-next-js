2d1a002b03dbfda686a55ffd6ccd1b4b085a7e64
Andre <andre_ranulfo@hotmail.com>
2025-05-08 20:18:57 -0300

    inorive isActive

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -1,34 +1,40 @@
+"use client"
 import React from 'react'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 export default function Menu() {
-
-  let navLinks = [
+  const navLinks = [
     { name: "Home", href: "/" },
     { name: "Blog", href: "/blog" },
-    { name: "Metadata", href: "/products" },
-    { name: "Products", href: "/products" },
     { name: "Metadata", href: "/metadata" },
+    { name: "Products", href: "/products" }
+  ];
 
+  // Correctly call usePathname()
+  const pathname = usePathname();
 
-
-  ]
   return (
-    <>
-      <ul className="main-menu flex flex-wrap gap-x-6 items-center">
-        {
-          navLinks.map((link, index) => (
-            <li key={index}>
-              <Link className="p-1 hover:bg-white hover:rounded-[5px] transition-all" href={link.href}>
-                {link.name}
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
+    <ul className="main-menu flex flex-wrap gap-x-6 items-center">
+      {navLinks.map((link, index) => {
+        
+        const isActive = pathname === link.href ||
+        pathname.startsWith(link.href) &&
+        pathname !== ""
 
-
-    </>
-
-  )
+        return (
+          <li key={index}>
+            <Link
+              href={link.href}
+              className={`p-1 transition-all hover:bg-white hover:rounded-[5px] ${
+                isActive ? 'font-bold  text-white bg-indigo-500 rounded-sm' : ''
+              }`}
+            >
+              {link.name}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
