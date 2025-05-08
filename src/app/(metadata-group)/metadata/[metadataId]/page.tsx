cca8d4f956415c7b845d238ff65e6c7078ec2f3a
Andre <andre_ranulfo@hotmail.com>
2025-05-08 13:57:15 -0300

    Dinamic - Metadata

--- src/app/(metadata-group)/metadata/[metadataId]/page.tsx
+++ src/app/(metadata-group)/metadata/[metadataId]/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+// 0. import Metadata
+import {Metadata} from 'next'
 
-export default async function MetadataId({
-    params,}:{
-        params:Promise<{metadataId:string}>
-    }) {
+
+// Dynamic Metadata
+//1. Declare type
+type Props = {
+    params:Promise<{metadataId:string}>
+}
+
+//2. generateMetadata()
+export const generateMetadata = async ( {params }: Props):
+ Promise<Metadata> => {
+    const metadataId = (await params).metadataId
+    return {
+        title : `Metadata #${metadataId}`,
+        description : `Description Metadata #${metadataId}`
+    }
+
+}
+
+
+//3. params : props
+export default async function MetadataId({params }: Props) {
 
 const metadataId = (await params).metadataId
 
