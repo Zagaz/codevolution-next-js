387396d4049aa687bb9a2585b1f1c1ad76a91a8a
Andre <andre_ranulfo@hotmail.com>
2025-05-08 10:44:39 -0300

    blogId working

--- src/app/(blog group)/blog/[blogId]/page.tsx
+++ src/app/(blog group)/blog/[blogId]/page.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 export default async function BlogId({
-  params,
-}: {
-  params: Promise<{ blogId: string }>
+    params
+}:{
+    params:Promise<{blogId:string}>
 }) {
-  const blogId = (await params).blogId;
+    const blogId = (await params).blogId
+    
+
+  
   return (
-    <>
-      <div className='py-2 px-4'>
-        <h3>BlogId:{blogId} </h3>
-      </div>
-    </>
+    <div>BlogId:{blogId}</div>
   )
 }
