7b0ed051cc3bfb4e4942335b89bf48fd381d585d
Andre <andre_ranulfo@hotmail.com>
2025-05-08 11:02:04 -0300

    format

--- src/app/(products group)/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/(products group)/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
 
-export default async function ProductDetails({ params }: { params: Promise<{ productId: string, reviewId: string }> }) {
+export default async function ProductDetails({
+  params,
+}: {
+  params: Promise<{ productId: string, reviewId: string }>
+}) {
   const { productId, reviewId } = await params;
 
   if (parseInt(reviewId) > 1000) { notFound() };
