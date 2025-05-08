4169d20f7ec2ddc30f894e4dea3d127b065109bc
Andre <andre_ranulfo@hotmail.com>
2025-05-08 20:02:16 -0300

    dynamic rendermenu

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -2,16 +2,33 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function Menu() {
+
+  let navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Blog", href: "/blog" },
+    { name: "Metadata", href: "/products" },
+    { name: "Products", href: "/products" },
+    { name: "Metadata", href: "/metadata" },
+
+
+
+  ]
   return (
     <>
-    <div className='menu'>
-    <Link className='' href="/">Home</Link>
-    <Link className='' href="/blog">Blog</Link>
-    <Link className='' href="/metadata">Metadata</Link>
-    <Link className='' href="/products">Products</Link>
-    </div>
+      <ul className="main-menu flex flex-wrap gap-x-6 items-center">
+        {
+          navLinks.map((link, index) => (
+            <li key={index}>
+              <Link className="p-1 hover:bg-white hover:rounded-[5px] transition-all" href={link.href}>
+                {link.name}
+              </Link>
+            </li>
+          ))
+        }
+      </ul>
+
 
     </>
-   
+
   )
 }
